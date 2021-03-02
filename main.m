3b8f7b4cb54c83cbe2931a49abc20b2db142f7c3
MennoLooman <49636185+MennoLooman@users.noreply.github.com>
2021-03-02 15:32:19 +0100

    cleaned and got basic moddeling down

--- main.m
+++ main.m
@@ -1,2 +1,23 @@
-clear variables; close all; clc;
-[A,B] = con_System();
+close all; clear all; clear variables; clear variables; clc;
+
+%% Initial state, input
+x0 = [1 1 1 1 1 1 1]';%initial state
+dt = 0.1; %sampling rate
+N = 100; %number of time steps
+t = 0:dt:dt*(N-1);
+u = ones(4,N); %input at every time step
+
+%% Continuous state space model
+[A_con,B_con] = con_System();
+D_con = zeros(7,4);
+C_con = eye(7);
+sys_con = ss(A_con,B_con,C_con,D_con);
+
+%% Discretise system (less accurate then lsim)
+sys_dis = c2d(sys_con,dt);
+A_dis = sys_dis.A;
+B_dis = sys_dis.B;
+
+%% Simulate system
+[~,~,x_dis] = lsim(sys_con,u,t,x0);
+%notice that x_dis is transposed
