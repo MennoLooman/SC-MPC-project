b257037462ed1c4d0c807c08e3b051f365435b2a
MennoLooman <49636185+MennoLooman@users.noreply.github.com>
2021-03-16 15:08:59 +0100

    almost working controller

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 close all; clear all; clear variables; clear variables; clc;
 N_inputs = 4;
+N_states = 7;
 
 %% Initial state, input
 x0 = [0.1 0.0 0.0 0.0 0.0 0.0 0.0]';%initial state
@@ -10,8 +11,8 @@ u = ones(N_inputs,N_steps); %input at every time step
 
 %% Continuous state space model
 [A_con,B_con] = con_System();
-D_con = zeros(7,N_inputs);
-C_con = eye(7);
+D_con = zeros(N_states,N_inputs);
+C_con = eye(N_states);
 sys_con = ss(A_con,B_con,C_con,D_con);
 
 %% Discretise system (less accurate then lsim)
@@ -20,7 +21,7 @@ A_dis = sys_dis.A;
 B_dis = sys_dis.B;
 
 %% State constraints in the form Fx <= state_bounds
-F = [eye(7); -eye(7)];
+F = [eye(N_states); -eye(N_states)];
 max_state_bounds = [1; 1; 1; 800; 1; 1; 1];
 state_bounds = [max_state_bounds; max_state_bounds];
 
@@ -37,26 +38,23 @@ Q = diag([500, 500, 500, 1e-7, 1, 1, 1]);
 R = diag([200, 200, 200, 1]);
 
 N_horizon = 2;
-P = 1*eye(7);
-x0_var = sdpvar(7,1);
+P = 1*eye(N_states);
+x0_var = sdpvar(1,1);
+x = sdpvar(repmat(N_states,1,N_horizon+1),repmat(1,1,N_horizon+1));
 u = sdpvar(repmat(N_inputs,1,N_horizon), repmat(1,1,N_horizon));
 
-%x = x0_var;
-Constraints = x(:,1)==x0;
-Objective = x(:,end)'*P*x(:,end);
+Constraints = [x{1}==x0_var]; 
+Objective = x{end}'*P*x{end};
 
 for k = 1:N_horizon
-   x = A_dis*x + B_dis*u{k};
-   Objective = Objective + 0.5*( x'*Q*x + u{k}'*R*u{k} );
-   Constraints = [Constraints, G*u{k} <= input_bounds, F*x <= state_bounds];
+   Objective = Objective + 0.5*( x{k}'*Q*x{k} + u{k}'*R*u{k} );
+   Constraints = [Constraints, G*u{k} <= input_bounds, F*x{k} <= state_bounds];
+   Constraints = [Constraints, x{k+1} == A_dis*x{k} + B_dis*u{k}];
 end
-sol = optimize([Constraints, x0_var==x0],Objective);
-if sol.problem == 1, error("problem in YALMIP: " + sol.info); end
-%% results YALMIP
-u_result = value(u);
-plot(u_result(1,:),[0,1]);
-hold on
-for k=2:4
-   plot(u_result(k,:),[0,1]);
-end
-hold off
+
+% !there might be an mistake in the controller input!
+%Controller = optimizer(Constraints,Objective,[],x0_var,[u{:}]);
+Controller = optimizer(Constraints,Objective,[],x0_var,u{1}); %do only want first column of u
+
+%% results from controller
+u_result = Controller{x0};
